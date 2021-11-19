699984c42212cad2b26793a1e09b72bfd092d1bb
Danilo Dias <danilo.dias@iteris.com.br>
2021-11-18 23:18:37 -0300

    delete any routes

--- src/config/routes/subRoutes.js
+++ src/config/routes/subRoutes.js
@@ -1,17 +1,17 @@
 export const subRoutesUsers = [
-  {
-    label: "Clínica",
-    path: "/register/clinicList",
-  },
+  // {
+  //   label: "Clínica",
+  //   path: "/register/clinicList",
+  // },
   {
     label: "Procedimento",
     path: "/register/procedureList",
   },
 
-  {
-    label: "Tipo de Clínica",
-    path: "/register/clinicListType",
-  },
+  // {
+  //   label: "Tipo de Clínica",
+  //   path: "/register/clinicListType",
+  // },
   {
     label: "Tipo de Consulta",
     path: "/register/appointmentsTypeList"
